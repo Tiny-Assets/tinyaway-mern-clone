c2e056b087adf25509d72fd08c5282b47c38f2a5
chloe <chloe.lee@bigtiny.com.sg>
2024-05-31 17:52:45 +0800

    Added expanded burger menu UI, links not working yet. Just mockup

--- frontend/src/components/sharedComponents/BurgerMenu/burgerMenuExpanded.jsx
+++ frontend/src/components/sharedComponents/BurgerMenu/burgerMenuExpanded.jsx
@@ -1,21 +1,35 @@
+// import social media icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';  
+import { faFacebookF, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'; 
+
+// render burger menu elements
 const BurgerMenuExpanded = ({ showMenu }) => {
     return(
         <div className="burgerMenu">
-            <div onClick={ showMenu }>close</div>
+            <div onClick={ showMenu }>X</div>
             <div>TINY HOUSE DESTINATIONS</div>
             <div>TINY AWAY ESCAPE</div>
-                <div className="subMenu">GRAMPIANS EDGE @ AUSTRALIA</div>
-                <div className="subMenu">LAZARUS ISLAND @ SINGAPORE</div>
+                <div className="subMenu">
+                    <div>GRAMPIANS EDGE @ AUSTRALIA</div>
+                    <div>LAZARUS ISLAND @ SINGAPORE</div>
+                </div>
             <div>TINY STORIES</div>
             <div>ABOUT US</div>
-                <div className="subMenu">OUR STORY</div>
-                <div className="subMenu">BECOME A HOST</div>
-                <div className="subMenu">CONTACT US</div>
-                <div className="subMenu">FAQ & CANCELLATIONS POLICY</div>
+                <div className="subMenu">
+                    <div>OUR STORY</div>
+                    <div>BECOME A HOST</div>
+                    <div>CONTACT US</div>
+                    <div>FAQ & CANCELLATIONS POLICY</div>
+                </div>
             <div>GIFT CARDS</div>
             <div>BLOG</div>
             <div>LOGIN</div>
-            
+            <div className="bookStayMenu">BOOK YOUR STAY</div>
+            <div className="socialIcons">
+                <FontAwesomeIcon className="burgerMenuIcon" icon={ faFacebookF }/>
+                <FontAwesomeIcon className="burgerMenuIcon" icon={ faInstagram }/>
+                <FontAwesomeIcon className="burgerMenuIcon" icon={ faYoutube }/>
+            </div>
         </div>
     )
 }
