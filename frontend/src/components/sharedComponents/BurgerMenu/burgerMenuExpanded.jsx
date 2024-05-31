cd832ef24df59f93788440b2156224f90c2ccafd
chloe <chloe.lee@bigtiny.com.sg>
2024-05-31 16:07:26 +0800

    Added expansion of burger menu, still working on the UI

--- frontend/src/components/sharedComponents/BurgerMenu/burgerMenuExpanded.jsx
+++ frontend/src/components/sharedComponents/BurgerMenu/burgerMenuExpanded.jsx
@@ -1,8 +1,23 @@
-export const burgerMenu = () => {
+const BurgerMenuExpanded = ({ showMenu }) => {
     return(
-        <div>
-            <h2>open</h2>
+        <div className="burgerMenu">
+            <div onClick={ showMenu }>close</div>
+            <div>TINY HOUSE DESTINATIONS</div>
+            <div>TINY AWAY ESCAPE</div>
+                <div className="subMenu">GRAMPIANS EDGE @ AUSTRALIA</div>
+                <div className="subMenu">LAZARUS ISLAND @ SINGAPORE</div>
+            <div>TINY STORIES</div>
+            <div>ABOUT US</div>
+                <div className="subMenu">OUR STORY</div>
+                <div className="subMenu">BECOME A HOST</div>
+                <div className="subMenu">CONTACT US</div>
+                <div className="subMenu">FAQ & CANCELLATIONS POLICY</div>
+            <div>GIFT CARDS</div>
+            <div>BLOG</div>
+            <div>LOGIN</div>
+            
         </div>
     )
 }
 
+export default BurgerMenuExpanded;
--- frontend/src/components/sharedComponents/Navbar/Navbar.jsx
+++ frontend/src/components/sharedComponents/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-
-// import GiftCards from '../../../pages/Giftcards/giftCards';
+// import required packages and files 
+import { useState } from 'react'; 
+import BurgerMenuExpanded from '../BurgerMenu/burgerMenuExpanded'; 
 
 // import assets & icons 
 import logoWhite from '../../../assets/logoWhite.png'; 
@@ -8,10 +8,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight, faBars, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons'; 
 
-const Navbar = () => {
 
+// render Navbar elements
+const Navbar = () => {
+    
+    // just for testing 
     const click = () => {
         console.log('target clicked'); 
+    }
+
+    // set state for burger menu 
+    const [showBurger, setShowBurger] = useState(false); 
+
+    function showMenu() {
+        setShowBurger(!showBurger); 
+        console.log('burger eaten'); 
     }
 
     return(
@@ -24,12 +35,10 @@ const Navbar = () => {
 
             {/* navigation bar */}
             <div className="navBar">
-
                 {/* logo side */}
                 <div className="taLogo">
                     <img src={ logoWhite } alt="logo"></img>
                 </div>
-
                 {/* links side */}
                 <div className="navLinks">
                     <div to="/giftCards" className="link" onClick={ click }>
@@ -50,13 +59,15 @@ const Navbar = () => {
                     <div className="cartIcon" onClick={ click }>
                         <FontAwesomeIcon icon={faCartShopping}/>
                     </div>
-
                     {/* burger menu within links side */}
-                    <button className="burgerIcon" onClick={ click }>
+                    <button className="burgerIcon" onClick={ showMenu }>
                         <FontAwesomeIcon icon={ faBars } />
                     </button>
                 </div>
             </div>
+            { showBurger &&
+                <BurgerMenuExpanded showMenu={ showMenu }/>
+            }
         </>
     )
 }
