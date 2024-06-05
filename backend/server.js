ae42730b4b8032e9307143c22d20f4d53f40906b
chloe <chloe.lee@bigtiny.com.sg>
2024-06-05 15:02:46 +0800

    Amended Backend Server File - Connection to MongoDB Sample Data Now Working

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,43 @@
 // import all packages as required 
 import { config } from 'dotenv'; 
-import express from 'express'; 
-import listings from './routes/listings.js'; 
-import mongoose from 'mongoose'; 
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 // load environment variables from .env file + declaring port 
 config(); 
 const port = process.env.PORT; 
 const mongoURI = process.env.MONGO_URI; 
 
-// express app 
-const app = express(); 
+// testing 
 
-// middleware
-app.use(express.json()); 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(mongoURI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
-// routes 
-app.use('/api/listings', listings); 
+// Testing Database Connection
+//   async function run() {
+//     try {
+//       // Connect the client to the server	(optional starting in v4.7)
+//       await client.connect();
+//       // Send a ping to confirm a successful connection
+//       await client.db("admin").command({ ping: 1 });
+//       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     } finally {
+//       // Ensures that the client will close when you finish/error
+//       await client.close();
+//     }
+//   }
+//   run().catch(console.dir);
 
-//connect to MongoDB Atlas 
-mongoose.connect(mongoURI)
-    .then(() => {
-        //listen for requests
-        app.listen( port, () => {
-            console.log('Connected to Atlas + Listening for Requests'); 
-        }); 
-    })
-    .catch((error) => {
-        console.log(error); 
-    }); 
+  async function listDB(client) {
+    const databasesList = await client.db().admin().listDatabases();
 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+  }
 
-
+  listDB(client); 
