76bf9245f5c68e0ee17bc1dd94414797e0935c1b
chloe <chloe.lee@bigtiny.com.sg>
2024-06-24 13:44:02 +0800

    Moved backend connection into frontend folder so that we do not need to run 2 servers to test development

---
+++ next-tinyaway/config/database.js
@@ -0,0 +1,32 @@
+require('dotenv').config(); 
+
+const { MongoClient } = require('mongodb');
+
+const URI = process.env.MONGO_URI; 
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function main(){
+    const client = new MongoClient(URI);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+console.log(URI); 
